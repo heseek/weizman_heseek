509e2c26d0786737b797703f566e387fd81dee13
heseek <freshmea@hotmail.com>
2021-04-24 14:20:52 +0900

    2-3

---
+++ 2-3.py
@@ -0,0 +1,22 @@
+# dict1 = {'name': '한사람', 'age': 22, 'gender': True}
+# print(dict1, type(dict1))
+#
+#
+# for key in dict1.keys():
+#     print(key, ':', dict1[key])
+#
+#
+# for value in dict1.values():
+#     print(value)
+#
+#
+# dict1['hobby'] = ['술', '춤', '잠']
+# dict1['weight'] = 78.65
+# print(dict1)
+
+
+setType1 = {1, 2, 3, 4}
+setType2 = {3, 4, 5, 6, 7, 8}
+
+
+unionset = setType1.union(setType2)

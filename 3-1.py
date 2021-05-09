ff66568d1b4ac331e0a62fe18f164ff916f65733
heseek <freshmea@hotmail.com>
2021-05-09 18:04:37 +0900

    3-1 수업

---
+++ 3-1.py
@@ -0,0 +1,30 @@
+# # 산술 연산자
+# a = 7
+# b = 4
+#
+# print('a + b = ', a + b)
+# print('a - b = ', a - b)
+# print('a * b = ', a * b)
+# print('a / b = ', a / b)
+# print('a % b = ', a % b)
+# print('a // b = ', a // b)
+# print('a ** b = ', a ** b)
+
+# # 관계 연산자
+# a = 3
+# b = 3
+#
+# print(a > b)
+# print(a >= b)
+# print(a < b)
+# print(a <= b)
+# print(a == b)
+# print(a != b)
+
+# a=1
+# for i in  range(1000):
+#     if a % 3 == 0 or a % 5 == 0:
+#         print(a, end = ' ')
+#     a += 1
+
+

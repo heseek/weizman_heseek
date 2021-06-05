2d6910b11b75583d61d081bce7d1e97f9167e388
heseek <freshmea@hotmail.com>
2021-06-05 14:52:23 +0900

    4-1

--- 3-4.py
+++ 3-4.py
@@ -1,25 +1,22 @@
-# # import matplotlib.pyplot as plt
-# # import matplotlib.font_manager as fm
-# #
-# # font_list = fm.findSystemFonts()
-# # path = font_list[font_list.index('C:\Windows\Fonts\malgun.ttf')]
-# # font_name = fm.FontProperties(fname=path, size=18).get_name()
-# # plt.rc('font', family=font_name)
-# #
-# # pets = ['개', '뱀', '괴물', '물고기', '햄스터', '고양이', '도마뱀', '와! 샌즈', '마크 드래곤`s', ]
-# # students = [56, 23, 1, 19, 25, 52, 31, 100, 55, ]
-# # colors = ['red', 'green', 'blue', 'yellow', 'cyan', 'orange', 'black', 'gold', 'green', ]
-# #
-# # plt.bar(pets, students, color=colors)
-# #
-# # plt.xlabel('애완동물')
-# # plt.ylabel('숫자')
-# # plt.title('사람들이 키우는 애완동물 수')
-# # plt.show()
+import matplotlib.font_manager as fm
+import matplotlib.pyplot as plt
 
+font_list = fm.findSystemFonts()
+path = font_list[font_list.index('C:\Windows\Fonts\malgun.ttf')]
+font_name = fm.FontProperties(fname=path, size=18).get_name()
+plt.rc('font', family=font_name)
 
-# import matplotlib.pyplot as plt
-#
-# countries = ['USA', 'GBR', 'China', 'Russia', 'Germany']
-# gold = [34, 53, 24, 54, 24]
-# colors = ['red', 'green', 'blue', 'yellow', 'cyan']
+pets = ['개', '뱀', '괴물', '물고기', '햄스터', '고양이', '도마뱀', '와! 샌즈', '마크 드래곤`s', ]
+students = [56, 23, 1, 19, 25, 52, 31, 100, 55, ]
+colors = ['red', 'green', 'blue', 'yellow', 'cyan', 'orange', 'black', 'gold', 'green', ]
+
+plt.bar(pets, students, color=colors)
+
+plt.xlabel('애완동물')
+plt.ylabel('숫자')
+plt.title('사람들이 키우는 애완동물 수')
+plt.show()
+
+countries = ['USA', 'GBR', 'China', 'Russia', 'Germany']
+gold = [34, 53, 24, 54, 24]
+colors = ['red', 'green', 'blue', 'yellow', 'cyan']
---
+++ 4-1.py
@@ -0,0 +1,97 @@
+# # # # # import tkinter as tk
+# # # # #
+# # # # # root = tk.Tk()
+# # # # #
+# # # # # labell = tk.Label(root, text="안녕하세요!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+# # # # # labell.pack()
+# # # # #
+# # # # # root.mainloop()
+# # # # import tkinter as tk
+# # # #
+# # # # root = tk.Tk()
+# # # # a = tk.Label(text="안녕")
+# # # # a.pack()
+# # # # a = tk.Label(text="+_+")
+# # # # a.pack()
+# # # # a = tk.Label(text="ㄴㅇㄱ")
+# # # # a.pack()
+# # # # a = tk.Label(text="ㅇ0ㅇ")
+# # # # a.pack()
+# # # # a = tk.Label(text="@_@")
+# # # # a.pack()
+# # # # a = tk.Label(text=";[")
+# # # # a.pack()
+# # # # a = tk.Label(text=":]")
+# # # # a.pack()
+# # # # a = tk.Label(text=";]")
+# # # # a.pack()
+# # # # a = tk.Label(text=":[")
+# # # # a.pack()
+# # # #
+# # # # b = tk.Entry()
+# # # # b.pack()
+# # # #
+# # # # root.mainloop()
+# # # import tkinter as tk
+# # #
+# # #
+# # # def okClick():
+# # #     name = b.get()
+# # #     print(name)
+# # #     a.config(text=name)
+# # #
+# # #
+# # # root = tk.Tk()
+# # #
+# # # a = tk.Label(text="*")
+# # # a.pack()
+# # #
+# # # b = tk.Entry()
+# # # b.pack()
+# # #
+# # # btn = tk.Button(root, text="1", command=okClick)
+# # # btn.pack()
+# # #
+# # # root.mainloop()
+# #
+# # import tkinter as tk
+# #
+# #
+# # def okClick():
+# #     name = b.get()
+# #     print(name)
+# #     a.config(text=name)
+# #
+# # def ret(event):
+# #     a.config(text='*')
+# #
+# # root = tk.Tk()
+# #
+# # a = tk.Label(text="*")
+# # a.pack()
+# #
+# # b = tk.Entry()
+# # b.pack()
+# #
+# # btn = tk.Button(root, text="1", command=okClick)
+# # btn.pack()
+# #
+# # a.bind('<Button-1>', ret)
+# # root.mainloop()
+#
+#
+# import tkinter as tk
+#
+# root = tk.Tk()
+#
+# root.title("윤해식")
+# root.geometry("640x400+100+100")
+# root.resizable(True, True)
+#
+# a = tk.Label(text="1")
+# a.pack()
+#
+# b = tk.Entry()
+# b.pack()
+#
+# root.mainloop()
